615daf85dc7e4baf1cc2ded86ba4d5c9b5d24830
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-04 20:10:16 +0530

    ADDED:using dict value printed arithmetic

--- arithmetic.sh
+++ arithmetic.sh
@@ -1,14 +1,18 @@
 #! /bin/bash -x
 
+declare -A operation
 
 read a
 read b
 read c
-  compute=$(( $a+$b*$c ));
-echo "compute=$compute"
- compute=$(( $a*$b+$c ));
- echo "compute=$compute"
- compute=$(( $c+$a/$b ));
- echo "compute=$compute"
- compute=$(( $a%$b+$c ));
- echo "compute=$compute"
+operation[compute1]="$(( $a+$b*$c ))"
+operation[compute2]="$(( $a*$b+$c ))"
+operation[compute3]="$(( $c+$a/$b ))"
+operation[compute4]="$(( $a%$b+$c ))"
+echo "compute1=" ${operation[compute1]}
+echo "compute2=" ${operation[compute2]}
+echo "compute3=" ${operation[compute3]}
+echo "compute4=" ${operation[compute4]}
+
+ 
+ 
